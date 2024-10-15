c2bd8eeeafd9b2fdf5f5cd32de1f422fef690d8f
Danila Bernat <danila.bernat@innowise.com>
2024-10-15 15:08:16 +0200

    Exercise-2 with bad naming

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,24 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    if (ratingArray.count < 3) { return 0; }
+    NSInteger result = [self countTeams:ratingArray] + [self countTeams:[ratingArray reverseObjectEnumerator].allObjects];
+    return result;
 }
 
+// use ratingArray[i] > ratingArray[j] > ratingArray[k]
+- (NSInteger)countTeams:(NSArray<NSNumber *>*)ratingArray {
+    int result = 0;
+    for (int i = 0; i < ratingArray.count - 2; i++) {
+        for (int j = i + 1; j < ratingArray.count - 1; j++) {
+            if (!(ratingArray[i].intValue > ratingArray[j].intValue)) { continue; }
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                if (ratingArray[j].intValue > ratingArray[k].intValue) {
+                    result++;
+                }
+            }
+        }
+    }
+    return result;
+}
 @end
